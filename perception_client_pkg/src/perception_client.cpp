799f8b5b13aa7d115a73a41f6b3fd9465e41d8ab
GeeF <andre.luebken@gmail.com>
2013-06-18 17:51:33 +0200

    perception_client now loops. queries perception server every second, plus prettier output in case no object has been perceived yet

--- perception_client_pkg/src/perception_client.cpp
+++ perception_client_pkg/src/perception_client.cpp
@@ -2,16 +2,12 @@
 #include "perception_group_msgs/AddTwoInts.h"
 #include "perception_group_msgs/GetClusters.h"
 #include <cstdlib>
-#include <iostream>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp> 
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "add_two_ints_client");
-  if (argc != 3)
-  {
-    ROS_INFO("usage: add_two_ints_client X Y");
-    return 1;
-  }
+  ros::init(argc, argv, "perception_client");
 
   ros::NodeHandle n;
 
@@ -19,24 +15,38 @@ int main(int argc, char **argv)
   perception_group_msgs::GetClusters clusterSrv;
   clusterSrv.request.s = "foobar";
 
-  if (clusterClient.call(clusterSrv))
+  // run until service gets shut down
+  while(true)
   {
-    // ROS_INFO("Sum: %ld", (long int)srv.response.sum);
-		ROS_INFO("Cluster Service call successful");
-    ROS_INFO("List size: %ld", (long int)clusterSrv.response.perceivedObjs.size() );
-    // ROS_INFO("List size: %ld", (long int)clusterSrv.response.perceivedObjs.size() );
-		for(int i=0; i < clusterSrv.response.perceivedObjs.size(); i++ ) {
-			ROS_INFO("ID of perceived object is: %d" ,clusterSrv.response.perceivedObjs[i].c_id);
-			ROS_INFO("Volume of perceived object is: %f" ,clusterSrv.response.perceivedObjs[i].c_volume);
-			ROS_INFO("Centroid(x) of perceived object is: %f , %f , %f " ,clusterSrv.response.perceivedObjs[i].c_centroid.x,clusterSrv.response.perceivedObjs[i].c_centroid.y,clusterSrv.response.perceivedObjs[i].c_centroid.z);
-
-			// geometry_msgs::Point centroid;
-		}
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service GetClusters");
-    return 1;
+    if (clusterClient.call(clusterSrv))
+    {
+      // ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+      ROS_INFO("Cluster Service call successful");
+      ROS_INFO("List size: %ld", (long int)clusterSrv.response.perceivedObjs.size() );
+      // wait a sec if list is empty. service may not be ready yet
+      if((long int)clusterSrv.response.perceivedObjs.size() == 0)
+      {
+        boost::this_thread::sleep(boost::posix_time::seconds(1));
+        ROS_INFO_STREAM("No objects perceived yet.");
+        continue;
+      }
+      // ROS_INFO("List size: %ld", (long int)clusterSrv.response.perceivedObjs.size() );
+      for(int i=0; i < clusterSrv.response.perceivedObjs.size(); i++ ) {
+        ROS_INFO("ID of perceived object is: %d", clusterSrv.response.perceivedObjs[i].c_id);
+        ROS_INFO("Volume of perceived object is: %f", clusterSrv.response.perceivedObjs[i].c_volume);
+        ROS_INFO("Centroid(x) of perceived object is: %f , %f , %f ",
+          clusterSrv.response.perceivedObjs[i].c_centroid.x,
+          clusterSrv.response.perceivedObjs[i].c_centroid.y,
+          clusterSrv.response.perceivedObjs[i].c_centroid.z);
+      }
+      ROS_INFO_STREAM("------------------------------------------------------------");
+    }
+    else
+    {
+      ROS_ERROR("Failed to call service GetClusters");
+      return 1;
+    }
+    boost::this_thread::sleep(boost::posix_time::seconds(1));
   }
 
   return 0;
